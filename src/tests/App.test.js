faa0d01f9bc8f1f41cd2548e32f83c6caddb7ecd
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-03-22 17:16:12 -0300

    Adicionado requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+
+import App from '../App';
+
+describe('Teste o componente <App.js />', () => {
+  test('O primeiro link deve possuir o texto Home', () => {
+    renderWithRouter(<App />);
+    // Acessa os elementos da tela
+    const firstLink = screen.getByRole('link', { name: /Home/i });
+    // Interagir com esses elementos - SE NECESSAARIO
+    // Fazer testes
+    expect(firstLink).toBeInTheDocument();
+  });
+  test('O segundo link deve possuir o texto About', () => {
+    renderWithRouter(<App />);
+    // Acessa os elementos da tela
+    const secondLink = screen.getByRole('link', { name: /About/i });
+    // Interagir com esses elementos - SE NECESSAARIO
+    // Fazer testes
+    expect(secondLink).toBeInTheDocument();
+  });
+
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    // Acessa os elementos da tela
+    const thirdLink = screen.getByRole('link', { name: /Pokémons/i });
+    // Interagir com esses elementos - SE NECESSAARIO
+    // Fazer testes
+    expect(thirdLink).toBeInTheDocument();
+  });
+});
