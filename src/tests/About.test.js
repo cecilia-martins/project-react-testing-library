bdacbbb206b256b76a67c2a620e393178ef5aeff
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-03-23 15:27:29 -0300

    Adicionado requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { createMemoryHistory } from 'history';
+// import userEvent from '@testing-library/user-event';
+
 import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
@@ -19,20 +19,29 @@ describe('Teste o componente <About.js />.', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/About');
     // Acessa os elementos
-    const title2 = screen.getByRole('heading', { level: 2 });
+    const title2 = screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
     // Interage com esses elementos - se necessario
     // Testa
     expect(title2).toBeInTheDocument();
   });
   test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/About');
     // Acessa os elementos
+    const pOne = screen.getByText(/This application simulates /i);
+    // const pTwo = screen.getByText(/One can filter Pokémons by type /i);
     // Interage com esses elementos - se necessario
     // Testa
+    expect(pOne).toBeDefined();
   });
   test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/About');
     // Acessa os elementos
+    const altImage = screen.getByAltText(/pokédex/i).src;
     // Interage com esses elementos - se necessario
     // Testa
+    expect(altImage).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
   // test('', () => {
 
