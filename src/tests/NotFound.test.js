9d85ae560d7dc759ebe89e5fba02a540c1ef1df9
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-03-23 17:58:54 -0300

    teste 2 - depois do run lint

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
 
-import renderWithRouter from '../services/renderWithRouter';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Teste o componente <NotFound.js />', () => {
