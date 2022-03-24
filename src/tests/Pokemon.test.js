3b8c04cbf57fa48f95f182358fc05371f80c92c5
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-03-24 11:36:54 -0300

    Commitando NOVAMENTE o 5 pq passa no STRIKER, no npm test... Mas não passou no github AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+
+// import reactRouterDom from 'react-router-dom';
+// import App from '../App';
+
+// describe('Teste o componente <Pokemon.js />', () => {
+//   test.only('Teste se é renderizado um card com informações de determinado pokémon',
+//     () => {
+//       reactRouterDom(<App />);
+//       const name = screen.getByText(/pikachu/i);
+//       // acessar os elementos
+//       // interagir com esses elementos
+//       // testar
+//       expect(name).toBeInTheDocument();
+//     });
+//   test('Teste se card do Pokémon indicado na Pokédex contém link de navegação', () => {
+//     // acessar os elementos
+//     // interagir com esses elementos
+//     // testar
+//   });
+//   test('Teste se é feito redirecionamento para página de detalhes do Pokémon', () => {
+//     // acessar os elementos
+//     // interagir com esses elementos
+//     // testar
+//   });
+//   test('Teste também se a URL exibida no navegador muda', () => {
+//     // acessar os elementos
+//     // interagir com esses elementos
+//     // testar
+//   });
+//   test('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {
+//     // acessar os elementos
+//     // interagir com esses elementos
+//     // testar
+//   });
+// });
