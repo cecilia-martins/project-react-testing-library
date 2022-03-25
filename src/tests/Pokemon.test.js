a73261a14d096e5c1e9f979039722a36700781bc
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-03-25 20:12:22 -0300

    Adicionado outra metade do 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -22,12 +22,18 @@ describe('Teste o componente <Pokemon.js />', () => {
       // console.log(value);
       const name = pokemonName.innerHTML;
       // console.log(name);
+      const type = pokemonType.innerHTML;
+      // console.log(type);
+
+      const typePokemon = screen.getAllByText(type)[0];
+      // console.log(typePokemon);
       const textWeight = screen.getByText(value);
       const imgPokemon = screen.getByAltText(`${name} sprite`);
       // interagir com esses elementos
+
       // testar
       expect(pokemonName).toBeInTheDocument();
-      expect(pokemonType).toBeInTheDocument();
+      expect(typePokemon).toBeInTheDocument();
       expect(pokemonWeight).toBeInTheDocument();
       expect(textWeight).toBeInTheDocument();
       expect(imgPokemon).toBeInTheDocument();
@@ -50,19 +56,46 @@ describe('Teste o componente <Pokemon.js />', () => {
       expect(url).toBe('/pokemons/25');
       expect(summary).toBeInTheDocument();
     });
-  // test.only('Teste se é feito redirecionamento para página de detalhes do Pokémon',
-  //   () => {
-  //     // acessar os elementos
-  //     // interagir com esses elementos
-  //     // testar
-  //   });
   test('Teste também se a URL exibida no navegador muda', () => {
+    const { history } = renderWithRouter(<App />);
+    // console.log(history);
     // acessar os elementos
+    const urlAtual = history.location.pathname;
+    const linkMoreDetails = screen.getByText(/More details/i);
+
+    expect(urlAtual).toBe('/');
+    expect(linkMoreDetails).toBeInTheDocument();
+
     // interagir com esses elementos
+    userEvent.click(linkMoreDetails);
+    const newUrl = history.location.pathname;
+    console.log(newUrl);
     // testar
+    expect(newUrl).toBe('/pokemons/25');
   });
   test('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {
+    renderWithRouter(<App />);
     // acessar os elementos
+    const linkMoreDetails = screen.getByText(/More details/i);
+    expect(linkMoreDetails).toBeInTheDocument();
+    userEvent.click(linkMoreDetails);
+    const noFavorite = screen.getByRole('checkbox', { checked: false });
+    expect(noFavorite).toBeInTheDocument();
+    userEvent.click(noFavorite);
+    const linkFavPokemons = screen.getByText(/Favorite Pokémons/i);
+    expect(linkFavPokemons).toBeInTheDocument();
+    userEvent.click(linkFavPokemons);
+    const pokemon = screen.getByTestId(nameId);
+    expect(pokemon).toBeInTheDocument();
+    const pokemonName = screen.getByTestId(nameId);
+    const name = pokemonName.innerHTML;
+    const altImage = screen.getByAltText(`${name} is marked as favorite`);
+    expect(altImage).toBeDefined();
+    // console.log(altImage);
+    const srcImage = altImage.src;
+    // console.log(srcImage);
+    expect(srcImage).toContain('/star-icon.svg');
+
     // interagir com esses elementos
     // testar
   });
